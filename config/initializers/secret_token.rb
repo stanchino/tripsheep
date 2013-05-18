78b89ee23fbea93c05ce5fdc4caa41b58a92a88d
Stanimir Dimitrov <stanchino@stan.sofia.hostway>
2013-05-18 15:04:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripsheep::Application.config.secret_token = 'd0b7610b36e995053c5409a9adaa8085ba19f21da2c93916deef954dd8c08239d516fa36af1f04854fab36b37d25bd5be6c92b56c8814aa7bfc045cf3289e6ec'
